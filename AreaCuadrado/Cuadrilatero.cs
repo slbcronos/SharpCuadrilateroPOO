c9200081904a91124ce807c8fe9edd2b0cbaf1ef
Salvador LB <slb_18@hotmail.com>
2022-02-02 00:08:27 -0600

    Creado en la Mac

---
+++ AreaCuadrado/Cuadrilatero.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace AreaCuadrado
+{
+    public class Cuadrilatero
+    {
+        //Atributos
+        private float lado1;
+        private float lado2;
+
+        //Genera constructor
+        public Cuadrilatero(float lado1, float lado2)
+        {
+            this.lado1 = lado1;
+            this.lado2 = lado2;
+        }
+
+        public Cuadrilatero(float lado1)
+        {
+            this.lado1 = this.lado2 = lado1;
+        }
+
+        //Metodos
+
+        public float getPerimetro()
+        {
+            float primetro = 2 * (lado1 + lado2);
+            return primetro;
+        }
+
+        public float getArea()
+        {
+            float area = lado1 * lado2;
+            return area;
+        }
+
+
+    }
+}
---
+++ AreaCuadrado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AreaCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float lado1;
+            float lado2;
+
+            Console.WriteLine("Digite el Lado 1: ");
+
+            lado1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el Lado 2: ");
+
+            lado2 = float.Parse(Console.ReadLine());
+
+            Cuadrilatero c1 = new Cuadrilatero(lado1,lado2);
+
+            if (lado1 == lado2)
+            {
+                c1 = new Cuadrilatero(lado1);
+            }
+            else
+            {
+                c1 = new Cuadrilatero(lado1, lado2);
+            }
+
+            //salida
+            Console.WriteLine("Primer Dato Dado es: " + lado1);
+            Console.WriteLine("Segundo Dato Dado es: " + lado2);
+            Console.WriteLine("-----------------------------------");
+
+            Console.WriteLine("El Perimetro es: " + c1.getPerimetro());
+            Console.WriteLine("El Area es: " + c1.getArea());
+
+
+        }
+    }
+}
